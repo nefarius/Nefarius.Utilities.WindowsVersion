851b150ac44e4bc9943698f3f981e7e4f7395699
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-05-08 17:48:40 +0200

    Fixed CI

--- src/Util/OsUpgradeDetection.cs
+++ src/Util/OsUpgradeDetection.cs
@@ -20,41 +20,35 @@ public class OsUpgradeDetection
     {
         get
         {
-            using (RegistryKey setupKey = Registry.LocalMachine.OpenSubKey(@"SYSTEM\Setup"))
+            using RegistryKey setupKey = Registry.LocalMachine.OpenSubKey(@"SYSTEM\Setup");
+            using RegistryKey upgradeKey = setupKey?.OpenSubKey("Upgrade");
+            // if this key isn't there, don't look any further
+            if (upgradeKey == null)
             {
-                using (RegistryKey upgradeKey = setupKey?.OpenSubKey("Upgrade"))
+                return false;
+            }
+
+            // only look at "Source OS (...)" sub-keys
+            foreach (string sosKeyName in setupKey.GetSubKeyNames().Where(v =>
+                         v.StartsWith("Source OS", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                using RegistryKey sosKey = setupKey.OpenSubKey(sosKeyName);
+                string productName = sosKey?.GetValue("ProductName") as string;
+
+                if (string.IsNullOrEmpty(productName))
                 {
-                    // if this key isn't there, don't look any further
-                    if (upgradeKey == null)
-                    {
-                        return false;
-                    }
+                    continue;
+                }
 
-                    // only look at "Source OS (...)" sub-keys
-                    foreach (string sosKeyName in setupKey.GetSubKeyNames().Where(v =>
-                                 v.StartsWith("Source OS", StringComparison.InvariantCultureIgnoreCase)))
-                    {
-                        using (RegistryKey sosKey = setupKey.OpenSubKey(sosKeyName))
-                        {
-                            string productName = sosKey?.GetValue("ProductName") as string;
+                // TODO: untested but should work
+                if (productName.StartsWith("Windows 7", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return true;
+                }
 
-                            if (string.IsNullOrEmpty(productName))
-                            {
-                                continue;
-                            }
-
-                            // TODO: untested but should work
-                            if (productName.StartsWith("Windows 7", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                return true;
-                            }
-
-                            if (productName.StartsWith("Windows 8", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                return true;
-                            }
-                        }
-                    }
+                if (productName.StartsWith("Windows 8", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return true;
                 }
             }
 
