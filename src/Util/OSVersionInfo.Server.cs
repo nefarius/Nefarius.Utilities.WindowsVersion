0de499c3f5f1d8a99e04e23db67b45e0047718bc
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-05-09 16:33:32 +0200

    Added IsWindowsServer

---
+++ src/Util/OSVersionInfo.Server.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Nefarius.Utilities.WindowsVersion.Util;
+
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static partial class OsVersionInfo
+{
+    private const int OS_ANYSERVER = 29;
+
+    /// <summary>
+    ///     True if Windows Server (any version) is detected, false otherwise.
+    /// </summary>
+    public static bool IsWindowsServer => IsOS(OS_ANYSERVER);
+
+    [DllImport("shlwapi.dll", SetLastError = true, EntryPoint = "#437")]
+    private static extern bool IsOS(int os);
+}
